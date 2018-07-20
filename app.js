c720eb7e7ffa68a47883cedcb3bc0c6e1fa49883
George gd Fekaris <gdfekaris@gmail.com>
2018-07-20 11:27:31 -0700

    Hard coded [set], [get], and [delete] functions. Also fixed bug in html.

--- app.js
+++ app.js
@@ -3,9 +3,22 @@ $(document).ready(function() {
   // $().on('click', function() {
 
   // });
+  
+  let $title = $(`.input-field-title`).text();
+  
+  $(`.store-btn`).on(`click`, function() {
+    //event.preventDefault(); //useful to know these two jQuery funcs
+    //even.stopPropagation();
+    
+    localStorage.setItem('hrext', 'three is the best');
+  });
 
-  $().on('click', function() {
+  $(`.get-btn`).on('click', function() {
+    console.log(localStorage.getItem('hrext'));
+  });
 
+  $(`.delete-btn`).on('click', function() {
+    localStorage.removeItem('hrext');
   });
 
 });
