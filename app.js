b97c23cfb922d4cc8b6fad0b0f346f2a5bde8fb9
George gd Fekaris <gdfekaris@gmail.com>
2018-07-24 14:44:12 -0700

    Restored save/get/delete functions. Added support for html and css code highlighting. Also restyled a bit.

--- app.js
+++ app.js
@@ -1,36 +1,57 @@
 $(document).ready(function() {
   const editor = ace.edit("editor");
-  editor.setTheme("ace/theme/monokai");
-  editor.session.setMode("ace/mode/javascript");
+  let editorTheme = editor.setTheme("ace/theme/monokai");
+  let editorLanguage = editor.session.setMode("ace/mode/javascript");
+  editorTheme;
+  editorLanguage;
+
+  //editorLanguage = editor.session.setMode("ace/mode/html")
+  //editorTheme = editor.setTheme("ace/theme/terminal")
   
   $(`.store-btn`).on(`click`, function() {
-    //event.preventDefault(); //useful to know these two jQuery funcs
-    //even.stopPropagation();
-    
     let $title = $(`.input-field-title`).val();
-    let $content = $(`.input-field-body`).val();
+    let content = editor.getValue();
     
-    localStorage.setItem($title, $content);
-
-    // $(`.input-field-title`).val(``);
-    // $(`.input-field-body`).val(``);
+    localStorage.setItem($title, content);
   });
 
   $(`.get-btn`).on('click', function() {
     let userFile = $(`.input-field-get`).val();
     let getThis = localStorage.getItem(userFile);
 
-    //$(`.display-retrieved`).html(`<p>${userFile} ${getThis}</p>`);
-
     $(`.input-field-title`).val(`${userFile}`);
-    $(`.input-field-body`).val(`${getThis}`);
+    editor.session.setValue(`${getThis}`);
 
+    $(`.input-field-get`).val(``);
   });
 
   $(`.delete-btn`).on('click', function() {
     let deleteValue = $(`.input-field-delete`).val();
 
     localStorage.removeItem(deleteValue);
+
+    $(`.input-field-delete`).val(``);
+  });
+
+  $(`#gquiv`).on('click', function() {
+    console.log('gquiv button works');
+  });
+
+  $(`#lang`).on('click', function() { 
+    let lang = $(`#lang`).text();
+
+    if (lang === `.js`) {
+      $(`#lang`).text(`.html`);
+      editorLanguage = editor.session.setMode("ace/mode/html")
+    }
+    if (lang === `.html`) {
+      $(`#lang`).text(`.css`);
+      editorLanguage = editor.session.setMode("ace/mode/css")
+    }
+    if (lang === `.css`) {
+      $(`#lang`).text(`.js`);
+      editor.session.setMode("ace/mode/javascript");
+    }
   });
 
   // $().on('click', function() {
